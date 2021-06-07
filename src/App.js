85b6dac1d86e8395a4061d33fb365a55b73e6888
Ali Behbahani <dr.ali.behbahani18@gmail.com>
2021-06-07 20:03:57 +0300

    update, will change later

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import "./App.css";
+import nftData from "./data.js";
 
 function App() {
+  const nftList = nftData.map((nft) => (
+    <div className="listElements" key={nft.id}>
+      <img className="listImages" src={nft.image} />
+      <p>{nft.name}</p>
+      <p>{nft.price}</p>
+    </div>
+  ));
+
   return (
     <div>
-      <h1 className="title title1">The NiFTy Shop</h1>
-      <h4 className="title title2">your go to NFT shop</h4>
-      <img
-        alt="nft"
-        src="https://www.forbes.com/advisor/wp-content/uploads/2021/04/NFT.jpeg-900x510.jpg"
-      />
+      <div>
+        <h1 className="title title1">The NiFTy Shop</h1>
+        <h4 className="title title2">your go to NFT shop</h4>
+        <img
+          className="image1"
+          alt="nft"
+          src="https://www.forbes.com/advisor/wp-content/uploads/2021/04/NFT.jpeg-900x510.jpg"
+        />
+      </div>
+      <div>{nftList}</div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,23 @@
+const nftData = [
+  {
+    id: 1,
+    name: "Beeple, Everydays—The First 5000 Days",
+    price: "$ 69M",
+    image: "https://news.artnet.com/app/news-upload/2021/02/CH-Beeples-NFT.jpg",
+  },
+  {
+    id: 2,
+    name: "bananaman, banana & tape",
+    price: "$ 146,2146",
+    image: "https://news.artnet.com/app/news-upload/2021/03/image-1024x974.png",
+  },
+  {
+    id: 3,
+    name: "Chris Torres, Nyan Cat",
+    price: "$ 561,000",
+    image:
+      "https://news.artnet.com/app/news-upload/2021/02/NYAN-CAT-ARTINTERVIEW-copy.jpg",
+  },
+];
+
+export default nftData;
